13305359acc8560cba5fd60b4d61f55845453311
Hank Su <253848296@qq.com>
2019-08-31 23:12:00 +0800

    update ignore

--- Scripts/S008_OP_FILES.py
+++ Scripts/S008_OP_FILES.py
@@ -18,7 +18,7 @@ print('os.path.basename', os.path.basename(r"D:\vim\src\auto\configure"))
 print('os.path.split', os.path.split(r"D:\vim\src\auto\configure"), os.path.split(r"D:\vim\src\auto\configure")[0])
 print('os.path.sep', os.getcwd().split(os.path.sep))
 print('os.path.getsize', os.path.getsize(r"D:\PycharmProjects\hankPylib\Scripts\S002_listSample.py"))
-print('os.path.listdir', os.listdir(os.getcwd()))
+print('os.listdir', os.listdir(os.getcwd()))
 print('\n-----------------calc total size-------------------')
 total_size = 0
 file_list = os.listdir(os.getcwd())
@@ -28,3 +28,36 @@ print(total_size)
 print('os.path.exists', os.path.exists(os.getcwd()))
 print('os.path.isfile', os.path.isfile(os.getcwd()))
 print('os.path.isdir', os.path.isdir(os.getcwd()))
+
+print('\n---------------read()--------------------')
+file1 = open(os.path.join(os.getcwd(), 'new_file'))
+old_content1 = file1.read()
+print(file1.read())
+file1.close()
+file2 = open(os.path.join(os.getcwd(), 'new_file'))
+print('file2.readlines()', file2.readlines())
+file2.close()
+file3 = open(os.path.join(os.getcwd(), 'new_file'))
+old_content2 = file3.readline()
+print('file3.readline()', old_content2)
+file3.close()
+
+print('\n---------------write()---------------------')
+file4 = open(os.path.join(os.getcwd(), 'new_file'), 'w')
+file4.write(old_content2)
+file4.close()
+file4 = open(os.path.join(os.getcwd(), 'new_file'), 'a')
+file4.write(old_content1)
+file4.close()
+
+file4 = open(os.path.join(os.getcwd(), 'new_file'), 'r')
+print(file4.read())
+file4.close()
+
+print('\n----------------shelve --------------------')
+import shelve
+
+shelfFile = shelve.open('Sources\shelfFile')
+cats = ['Zophie', 'Pooka', 'Simon']
+shelfFile['cats'] = cats
+shelfFile.close()
