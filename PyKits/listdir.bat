d35c826e859a514f30beb8979bf179f8963a52d5
Hank Su <hank_h_su@manulife.com>
2019-09-06 18:03:16 +0800

    GIT_study.xmind
    git_s

--- PyKits/listdir.bat
+++ PyKits/listdir.bat
@@ -1,3 +1,3 @@
-@rem @py.exe -3 listdir.py %1 %2
-@python.exe "D:\PycharmProjects\hankPylib\PyKits\listdir.py" %1
+@py.exe -3 listdir.py %1 %2
+@rem @python.exe "D:\PycharmProjects\hankPylib\PyKits\listdir.py" %1
 @pause
